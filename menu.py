5f71e6d81628a77ade2eb53d9abba1cd5fd3a770
Dylan Fierro <dfierro125[200~@~gmail.com>
2025-09-03 19:40:31 -0500

    Parte Dylan Fierro

--- menu.py
+++ menu.py
@@ -1,65 +1,28 @@
-#Calculadora
+#Calculadora github
+
 import math
 
-operacion=str(input("¿Que operacion quiere? suma, resta, multiplicacion, division: "))
-#suma
-def suma(num1,num2):
-        num1=(float(input("Digite un numero: ")))
-        num2=(float(input("Digite un numero: ")))
-        suma= num1+num2
-        
-        if operacion == suma:
-            print ("Su refultado fue: ",suma)
-def resta(num1,num2):
-        num1=(float(input("Digite un numero: ")))
-        num2=(float(input("Digite un numero: ")))
-        resta= num1-num2
+def multiplicacion (num1, num2):
+    return num1*num2
 
+def division (num1, num2):
+    if num2 != 0:
+        return num1/num2 
+    else:
+        return "No es posible dividir por 0"
+    
+print("Elija la operacion que desea hacer:suma, resta, multiplicacion, division")
 
-if operacion == resta: 
-    print ("Su resultado fue: ",resta)
+operacion=input("Que operacion desea hacer")
 
+num1=float(input("Digite su primer numero: "))
+num2=float(input("Digite su segundo numero: "))
 
+if operacion == "multiplicacion":
+    print("El resultado de su multiplicacion es: ",multiplicacion(num1 , num2))
 
+elif operacion == "division":
+    print("El resultado de su division es:",division(num1,num2))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-num1=(float(input("Digite un numero: ")))
-num2=(float(input("Digite un numero: ")))
-suma= num1+num2
-
-print("Su suma fue: ",suma)
+else:
+    print("Su operacion no es valida")
