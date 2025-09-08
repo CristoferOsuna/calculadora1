1121d209b87e768eda14272998a4cd280caaeabe
CristoferOsuna <osunabaragancrustoferesmit@gmail.com>
2025-09-08 04:36:31 -0500

    +,-,*,/

--- menu.py
+++ menu.py
@@ -1,5 +1,6 @@
 #Calculadora github
 
+#Suma y Resta 
 
 def sumar(a, b):
     return a + b
@@ -25,3 +26,31 @@ while True:
         num1 = float(input("Ingresa el primer número: "))
         num2 = float(input("Ingresa el segundo número: "))
         print(f"Resultado:= {restar(num1, num2)}")
+
+
+        #Multiplicacion y Division 
+
+def multiplicacion (num1, num2):
+    return num1*num2
+
+def division (num1, num2):
+    if num2 != 0:
+        return num1/num2 
+    else:
+        return "No es posible dividir por 0"
+    
+print("Elija la operacion que desea hacer:suma, resta, multiplicacion, division")
+
+operacion=input("Que operacion desea hacer")
+
+num1=float(input("Digite su primer numero: "))
+num2=float(input("Digite su segundo numero: "))
+
+if operacion == "multiplicacion":
+    print("El resultado de su multiplicacion es: ",multiplicacion(num1 , num2))
+
+elif operacion == "division":
+    print("El resultado de su division es:",division(num1,num2))
+
+else:
+    print("Su operacion no es valida")
