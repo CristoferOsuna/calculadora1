2ffdd0b7622f401d17126a80e02fd7cee80a9440
CristoferOsuna <osunabaragancrustoferesmit@gmail.com>
2025-09-08 04:22:24 -0500

    Suma y Resta

--- menu.py
+++ menu.py
@@ -1,28 +1,27 @@
 #Calculadora github
 
-import math
 
-def multiplicacion (num1, num2):
-    return num1*num2
+def sumar(a, b):
+    return a + b
 
-def division (num1, num2):
-    if num2 != 0:
-        return num1/num2 
-    else:
-        return "No es posible dividir por 0"
-    
-print("Elija la operacion que desea hacer:suma, resta, multiplicacion, division")
+def restar(a, b):
+    return a - b
 
-operacion=input("Que operacion desea hacer")
+def menu():
+    print("Calculadora")
+    print("1. Sumar")
+    print("2. Restar")
 
-num1=float(input("Digite su primer numero: "))
-num2=float(input("Digite su segundo numero: "))
+while True:
+    menu()
+    opcion = input("Elige una opción (1-2): ")
 
-if operacion == "multiplicacion":
-    print("El resultado de su multiplicacion es: ",multiplicacion(num1 , num2))
+    if opcion == "1":
+        num1 = float(input("Ingresa el primer número: "))
+        num2 = float(input("Ingresa el segundo número: "))
+        print(f"Resultado:= {sumar(num1, num2)}")
 
-elif operacion == "division":
-    print("El resultado de su division es:",division(num1,num2))
-
-else:
-    print("Su operacion no es valida")
+    elif opcion == "2":
+        num1 = float(input("Ingresa el primer número: "))
+        num2 = float(input("Ingresa el segundo número: "))
+        print(f"Resultado:= {restar(num1, num2)}")
